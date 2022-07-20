912ff6a473c83de0575f4e0711f285081a3c0332
André Gomes <102058718+aaandregomes@users.noreply.github.com>
2022-07-19 21:54:35 -0300

    Delete test006.py

--- test006.py
+++
@@ -1,6 +0,0 @@
-nome =str(input('Qual é seu nome?'))
-print('Prazer em te conhecer {:20}!'.format(nome))
-print('Prazer em te conhecer {:>20}!'.format(nome))
-print('Prazer em te conhecer {:^20}!'.format(nome))
-print('Prazer em te conhecer {:*^20}!'.format(nome))
-
